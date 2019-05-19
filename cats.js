686971d511a98369b853d93909da3d97cfd0cc11
Maite Arp <arp.maite@gmail.com>
2019-05-18 17:50:02 -0700

    addedjs

---
+++ cats.js
@@ -0,0 +1,17 @@
+
+console.log('This is my cat');
+
+var catQuestion = prompt('Do you like cats?');
+console.log (catQuestion);
+
+if (catQuestion === 'yes') {
+document.write('<h3>Welcome to the cat page!</h3>')
+} else if (catQuestion === 'no') {
+document.write('<h3>Welcome anyway...</h3>')
+} else {
+    document.write('<h3>Welcome!</h3>')
+}
+
+
+
+
